664ebefd9547683ade2e440938cbc05e827ba22a
Hardik Biyani <38585920+HB936@users.noreply.github.com>
2025-05-21 02:08:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,21 +7,33 @@ const server = express();
 server.use(express.json());
 
 server.use(cors({
-    origin: ['https://blog-maker-vercel.vercel.app']
+  origin: ['http://localhost:5173']
 }));
 
 server.use('/api/blog', BlogsRouter);
 
-// Remove app.listen and instead export the handler for Vercel
+// A variable to hold the connected server
+let isConnected = false;
+
+// Middleware to block requests until MongoDB is connected
+server.use((req, res, next) => {
+  if (!isConnected) {
+    return res.status(500).send({ msg: "MongoDB not connected", status: 0 });
+  }
+  next();
+});
+
 mongoose.connect(
-    "mongodb+srv://x4deadshot:mongodbpassword@cluster0.h7haiys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-        dbName: "RevoltronX"
-    }
+  "mongodb+srv://x4deadshot:mongodbpassword@cluster0.h7haiys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  {
+    dbName: "RevoltronX"
+  }
 ).then(() => {
-    console.log("Connected to MongoDB");
-}).catch(() => {
-    console.log("Unable to connect to database");
+  console.log("Connected to MongoDB Atlas");
+  isConnected = true;
+}).catch((err) => {
+  console.error("MongoDB connection error:", err);
+  isConnected = false;
 });
 
 module.exports = server;
