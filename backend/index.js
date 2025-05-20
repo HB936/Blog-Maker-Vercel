227e01c9dd9d3f56f90f86e9c54af25412f9f5b4
Hardik Biyani <38585920+HB936@users.noreply.github.com>
2025-05-21 01:48:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,25 +8,20 @@ server.use(express.json());
 
 server.use(cors({
     origin: ['http://localhost:5173']
-}))
+}));
 
 server.use('/api/blog', BlogsRouter);
 
-
+// Remove app.listen and instead export the handler for Vercel
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/",
+    "mongodb+srv://x4deadshot:hardikbhai92006@cluster0.h7haiys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     {
         dbName: "RevoltronX"
     }
-).then(
-    () => {
-        server.listen(
-            5000,
-            () => console.log('Server Started')
-        )
-    }
-).catch(
-    () => {
-        console.log("Unable to connect to database"); 
-    }
-)
+).then(() => {
+    console.log("Connected to MongoDB");
+}).catch(() => {
+    console.log("Unable to connect to database");
+});
+
+module.exports = server;
